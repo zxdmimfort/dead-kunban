230363990ac22af348aeea7e209f572ddfc5642f
AOI-dev <olegzandroff@gmail.com>
2025-02-16 15:16:28 +0300

    clean up models.py code

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 
 
 class HistoryRecord(BaseModel):
@@ -11,28 +10,17 @@ class KanbanCard(BaseModel):
     id: int | None = None
     title: str | None = None
     description: str | None = None
-    assignee: str | None = None  # Equivalent to required=False in Marshmallow
+    assignee: str | None = None
     status: str | None = None
     dueDate: str | None = None
-    priority: str | None = None  # Equivalent to required=False in Marshmallow
+    priority: str | None = None
     createdAt: str | None = None
-    period: int = Field(default=-1)  # Equivalent to missing=-1 in Marshmallow
-    history: (
-        list[HistoryRecord] | None
-    ) = []  # Nested field with many=True in Marshmallow
-
-    cooldown: Optional[str] = Field(
-        default=""
-    )  # Equivalent to missing='' in Marshmallow
-    history_as_string: str | None = Field(
-        default=""
-    )  # Equivalent to missing='' in Marshmallow
-    days_till_todo: int | None = Field(
-        default=-1
-    )  # Equivalent to missing=-1 in Marshmallow
-    hours_till_todo: int | None = Field(
-        default=-1
-    )  # Equivalent to missing=-1 in Marshmallow
+    period: int = Field(default=-1)
+    history: list[HistoryRecord] | None = []
+    cooldown: str | None = Field(default="")
+    history_as_string: str | None = Field(default="")
+    days_till_todo: int | None = Field(default=-1)
+    hours_till_todo: int | None = Field(default=-1)
 
 
 class Kanban(BaseModel):
